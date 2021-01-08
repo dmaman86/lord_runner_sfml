92aebaad1304e568713a4d470b8ac429ccf5c9b3
DanielCohen94 <dutzcohen@gmail.com>
2021-01-08 02:53:17 +0200

    board size

--- src/models/Object.cpp
+++ src/models/Object.cpp
@@ -2,18 +2,27 @@
 
 Object::Object(sf::Vector2f position, sf::Vector2f size, sf::Texture* txt)
 {
+	/*
 	m_rec = new sf::Sprite;
 
 	m_rec->setPosition(sf::Vector2f(position.x * size.x, position.y * size.y));
 
     m_rec->setTexture(*txt);
-	//m_rec->setScale(size.x / 100, size.y / 100);
-	//m_rec->setColor(sf::Color::Red);
-	
+
 	m_rec->scale(size.x / txt->getSize().x, size.y / txt->getSize().y);
 	m_rec->setOrigin(size.x / 2.f, size.y / 2.f);
-	//sf::RectangleShape r;
+	*/
+	m_rec = new sf::Sprite;
+
+	m_rec->setTexture(*txt);
 	
+	m_rec->setScale(sf::Vector2f(size.x / txt->getSize().x, size.y / txt->getSize().y));//15 / size.x, 25 / size.y);
+
+	m_rec->setOrigin(sf::Vector2f(txt->getSize() / 2u));
+
+	m_rec->setPosition
+	(sf::Vector2f((position.x * size.x) + size.x / 2u, (position.y * size.y) + size.y / 2u));
+
 }
 
 Object::~Object()
